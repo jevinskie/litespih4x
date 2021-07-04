f8046715430d242ece323562d50d75f3c093cfc6
Jevin Sweval <jevinsweval@gmail.com>
2021-07-03 23:56:46 -0700

    sim base

--- examples/sim.py
+++ examples/sim.py
@@ -1,9 +1,125 @@
 #!/usr/bin/env python3
 
-from litejtag_ext.hello import JTAGHello
+# Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
 
 import argparse
-import sys
 
-if __name__ == '__main__':
-    sys.exit(0)
+from migen import *
+
+from litex.build.generic_platform import *
+from litex.build.sim import SimPlatform
+from litex.build.sim.config import SimConfig
+
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.interconnect import stream
+
+from liteeth.phy.model import LiteEthPHYModel
+from liteeth.mac import LiteEthMAC
+from liteeth.core.arp import LiteEthARP
+from liteeth.core.ip import LiteEthIP
+from liteeth.core.udp import LiteEthUDP
+from liteeth.core.icmp import LiteEthICMP
+from liteeth.core import LiteEthUDPIPCore
+from liteeth.frontend.etherbone import LiteEthEtherbone
+from liteeth.frontend.stream import LiteEthStream2UDPTX
+from liteeth.common import convert_ip
+
+from litejtag_ext.hello import TickerZeroToMax, BeatTickerZeroToMax
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    ("sys_clk", 0, Pins(1)),
+    ("sys_rst", 0, Pins(1)),
+    ("ticker_zero_to_max", 0,
+        Subsignal("tick", Pins(1)),
+        Subsignal("counter", Pins(32)),
+    ),
+    ("ticker_zero_to_max_from_freq", 0,
+        Subsignal("tick", Pins(1)),
+        Subsignal("counter", Pins(32)),
+    ),
+    ("beat_ticker", 0,
+        Subsignal("tick", Pins(1)),
+        Subsignal("tick_a", Pins(1)),
+        Subsignal("counter_a", Pins(32)),
+        Subsignal("tick_b", Pins(1)),
+        Subsignal("counter_b", Pins(32)),
+     ),
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(SimPlatform):
+    def __init__(self):
+        SimPlatform.__init__(self, "SIM", _io)
+
+# Bench SoC ----------------------------------------------------------------------------------------
+
+class BenchSoC(SoCCore):
+    def __init__(self, sim_debug=False, trace_reset_on=False, **kwargs):
+        platform     = Platform()
+        sys_clk_freq = int(1e6)
+
+        # SoCMini ----------------------------------------------------------------------------------
+        SoCMini.__init__(self, platform, clk_freq=sys_clk_freq,
+            ident          = "LiteEth stream Simulation",
+            ident_version  = True
+        )
+
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = CRG(platform.request("sys_clk"))
+
+        # Ticker A
+        self.submodules.ticker_a = TickerZeroToMax(self.platform.request("ticker_zero_to_max"), max_cnt=15)
+        # Ticker B
+        self.submodules.ticker_b = BeatTickerZeroToMax(self.platform.request("beat_ticker"), max_cnt_a=5, max_cnt_b=7)
+
+        if sim_debug:
+            platform.add_debug(self, reset=1 if trace_reset_on else 0)
+        else:
+            self.comb += platform.trace.eq(1)
+
+        # SRAM -------------------------------------------------------------------------------------
+        # self.add_ram("sram", 0x20000000, 0x1000)
+
+# Main ---------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteJTAG Simulation")
+    parser.add_argument("--trace",                action="store_true",     help="Enable Tracing")
+    parser.add_argument("--trace-fst",            action="store_true",     help="Enable FST tracing (default=VCD)")
+    parser.add_argument("--trace-start",          default="0",             help="Time to start tracing (ps)")
+    parser.add_argument("--trace-end",            default="-1",            help="Time to end tracing (ps)")
+    parser.add_argument("--trace-exit",           action="store_true",     help="End simulation once trace finishes")
+    parser.add_argument("--sim-end",              default="-1",            help="Time to end simulation (ps)")
+    parser.add_argument("--sim-debug",            action="store_true",     help="Add simulation debugging modules")
+    args = parser.parse_args()
+    try:
+        args.trace_start = int(args.trace_start)
+    except:
+        args.trace_start = int(float(args.trace_start))
+    try:
+        args.trace_end = int(args.trace_end)
+    except:
+        args.trace_end = int(float(args.trace_end))
+
+    sim_config = SimConfig()
+    sim_config.add_clocker("sys_clk", freq_hz=1e6)
+
+    soc     = BenchSoC(sim_debug=args.sim_debug, trace_reset_on=args.trace_start > 0 or args.trace_end > 0)
+    builder = Builder(soc, csr_csv="csr.csv")
+    builder.build(
+        sim_config  = sim_config,
+        trace       = args.trace,
+        trace_fst   = args.trace_fst,
+        trace_start = args.trace_start,
+        trace_end   = args.trace_end,
+        trace_exit  = args.trace_exit,
+        sim_end     = args.sim_end
+    )
+
+if __name__ == "__main__":
+    main()
--- litejtag_ext/hello.py
+++ litejtag_ext/hello.py
@@ -1,4 +1,69 @@
+# Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from __future__ import annotations
+
 from rich import print
+
+from migen import *
+
+class TickerZeroToMax(Module):
+    def __init__(self, pads: Record, max_cnt: int):
+        self.counter = counter = Signal(max=max_cnt)
+        self.tick = tick = Signal()
+        print(pads)
+        assert counter.nbits <= pads.counter.nbits
+
+        self.comb += pads.tick.eq(tick)
+        self.comb += pads.counter.eq(counter)
+        self.sync += \
+            If(counter == max_cnt,
+               tick.eq(1),
+               counter.eq(0)
+            ).Else(
+                tick.eq(0),
+                counter.eq(counter + 1)
+            )
+
+    @classmethod
+    def from_period(cls, pads: Record, sys_clk_freq: int, ticker_period: float) -> TickerZeroToMax:
+        counter_preload = int(sys_clk_freq * ticker_period) - 1
+        return cls(pads, counter_preload)
+
+    @classmethod
+    def from_freq(cls, pads: Record, sys_clk_freq: int, ticker_freq: int) -> TickerZeroToMax:
+        return cls.from_period(pads, sys_clk_freq, 1 / ticker_freq)
+
+
+class BeatTickerZeroToMax(Module):
+    def __init__(self, pads: Record, max_cnt_a: int, max_cnt_b: int):
+        self.tick = tick = Signal()
+
+        pads_a = Record([
+            ("tick", pads.tick_a),
+            ("counter", pads.counter_a),
+        ], "ticker_a")
+        self.submodules.ticker_a = TickerZeroToMax(pads_a, max_cnt_a)
+
+        pads_b = Record([
+            ("tick", pads.tick_b),
+            ("counter", pads.counter_b),
+        ], "ticker_b")
+        self.submodules.ticker_b = TickerZeroToMax(pads_b, max_cnt_b)
+
+        self.comb += pads.tick.eq(tick)
+        self.comb += tick.eq(self.ticker_a.tick & self.ticker_b.tick)
+
+    @classmethod
+    def from_period(cls, pads: Record, sys_clk_freq: int, ticker_period_a: float, ticker_period_b: float) -> BeatTickerZeroToMax:
+        counter_preload_a = int(sys_clk_freq * ticker_period_a) - 1
+        counter_preload_b = int(sys_clk_freq * ticker_period_b) - 1
+        return cls(pads, max_cnt_a=counter_preload_a, max_cnt_b=counter_preload_b)
+
+    @classmethod
+    def from_freq(cls, pads: Record, sys_clk_freq: int, ticker_freq_a: int, ticker_freq_b: int) -> BeatTickerZeroToMax:
+        return cls.from_period(pads, sys_clk_freq, 1 / ticker_freq_a, 1 / ticker_freq_b)
+
 
 class JTAGHello:
     pass
