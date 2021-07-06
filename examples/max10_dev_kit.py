041a6785c4a0d39b2a5be472a62fcf1d45e53c3f
Jevin Sweval <jevinsweval@gmail.com>
2021-07-05 18:01:50 -0700

    MAX10: .sof not .bit

--- examples/max10_dev_kit.py
+++ examples/max10_dev_kit.py
@@ -92,7 +92,7 @@ def main():
 
     if args.load:
         prog = soc.platform.create_programmer()
-        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".bit"))
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".sof"))
 
 if __name__ == "__main__":
     main()
