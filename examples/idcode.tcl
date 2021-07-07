441cc170a924cd6379296d2b760bd550d45531b8
Jevin Sweval <jevinsweval@gmail.com>
2021-07-06 19:14:19 -0700

    jtag test tcl scripts

---
+++ examples/idcode.tcl
@@ -0,0 +1,37 @@
+# List all available programming hardware, and select the USB-Blaster.
+# (Note: this example assumes only one USB-Blaster is connected.)
+puts "Programming Hardware:"
+foreach hardware_name [get_hardware_names] {
+	puts $hardware_name
+	if { [string match "USB-Blaster*" $hardware_name] } {
+		set usbblaster_name $hardware_name
+	}
+}
+puts "\nSelect JTAG chain connected to $usbblaster_name.\n";
+
+# List all devices on the chain, and select the first device on the chain.
+puts "\nDevices on the JTAG chain:"
+foreach device_name [get_device_names -hardware_name $usbblaster_name] {
+	puts $device_name
+	if { [string match "@1*" $device_name] } {
+		set test_device $device_name
+	}
+}
+puts "\nSelect device: $test_device.\n";
+
+# Open device 
+open_device -hardware_name $usbblaster_name -device_name $test_device
+
+# Retrieve device id code.
+# IDCODE instruction value is 6; The ID code is 32 bits long.
+
+# IR and DR shift should be locked together to ensure that other applications 
+# will not change the instruction register before the id code value is shifted
+# out while the instruction register is still holding the IDCODE instruction.
+device_lock -timeout 10000
+device_ir_shift -ir_value 6 -no_captured_ir_value
+puts "IDCODE: 0x[device_dr_shift -length 32 -value_in_hex]"
+device_unlock
+
+# Close device
+close_device
---
+++ examples/irdrscan-openocd-blaster2.tcl
@@ -0,0 +1,20 @@
+source [find interface/altera-usb-blaster2.cfg]
+source [find fpga/altera-10m50.cfg]
+
+gdb_port disabled
+tcl_port disabled
+telnet_port disabled
+init
+
+irscan 10m50.tap 0xc
+
+puts "  DEADBEEF 32 RES: 0x[drscan 10m50.tap 32 0xDEADBEEF]"
+
+puts "00DEADBEEF 40 RES: 0x[drscan 10m50.tap 40 0x00DEADBEEF]"
+
+puts "00DEADBEEF 64 RES: 0x[drscan 10m50.tap 64 0x00000000DEADBEEF]"
+
+puts "00DEADBEEF 80 RES: 0x[drscan 10m50.tap 80 0x000000000000DEADBEEF]"
+
+
+shutdown
