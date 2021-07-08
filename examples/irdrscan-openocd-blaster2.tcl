2ce5b6568e9d8886896318d9b281ced5c0ba2ca7
Jevin Sweval <jevinsweval@gmail.com>
2021-07-07 22:15:47 -0700

    wip

--- examples/irdrscan-openocd-blaster2.tcl
+++ examples/irdrscan-openocd-blaster2.tcl
@@ -6,7 +6,10 @@ tcl_port disabled
 telnet_port disabled
 init
 
-irscan 10m50.tap 0xc
+irscan 10m50.tap 0xe
+
+puts "Waiting for Any Key"
+gets stdin
 
 puts "  DEADBEEF 32 RES: 0x[drscan 10m50.tap 32 0xDEADBEEF]"
 
@@ -15,6 +18,5 @@ puts "00DEADBEEF 40 RES: 0x[drscan 10m50.tap 40 0x00DEADBEEF]"
 puts "00DEADBEEF 64 RES: 0x[drscan 10m50.tap 64 0x00000000DEADBEEF]"
 
 puts "00DEADBEEF 80 RES: 0x[drscan 10m50.tap 80 0x000000000000DEADBEEF]"
-
 
 shutdown
